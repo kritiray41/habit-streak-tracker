141a51d2493c3818d1a12e2b8aa49e130b1df285
kritiray41 <kritiray618@gmail.com>
2026-03-28 10:58:22 +0530

    Added HabitProvider state management

---
+++ lib/providers/habit_provider.dart
@@ -0,0 +1,12 @@
+class HabitProvider extends ChangeNotifier {
+
+  List habits = [];
+
+  void addHabit(String name) {}
+
+  void toggleHabit(String id) {}
+
+  int calculateStreak() {
+    return 0;
+  }
+}
